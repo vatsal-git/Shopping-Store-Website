feaba8b22d878dd85a533914e0fd89b517286de7
Vatsal Patel <69387444+vatsal-git@users.noreply.github.com>
2021-02-18 10:58:24 +0530

    add style

--- Admin/login.php
+++ Admin/login.php
@@ -1,30 +1,57 @@
 <?php include('account server.php') ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-  <title>Login</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" href="img/trac-icon.svg">
+    <title>TRAC online fashion</title>
+
+    <!-- FONT -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;300;400;500;600;700&display=swap"
+        rel="stylesheet">
+
+    <!-- STYLESHEET -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/main-style.css">
 </head>
+
 <body>
-  <div class="header">
-  	<h2>Login</h2>
-  </div>
-	 
-  <form method="post" action="login.php">
-  	<?php include('account error.php'); ?>
-  	<div class="input-group">
-  		<label>Username</label>
-  		<input type="text" name="username" >
-  	</div>
-  	<div class="input-group">
-  		<label>Password</label>
-  		<input type="password" name="password">
-  	</div>
-  	<div class="input-group">
-  		<button type="submit" class="btn" name="login_user">Login</button>
-  	</div>
-  	<p>
-  		Not yet a member? <a href="register.php">Sign up</a>
-  	</p>
-  </form>
+    <div id="login-section">
+
+        <div class="login-form-container">
+            <div class="login-logo">
+                <img src="../img/trac-logo.svg" alt="logo">
+            </div>
+            <h2>Log in</h2>
+            <form>
+                <?php include('account error.php'); ?>
+                
+                <div class="mb-3">
+                    <input type="text" name="username" class="form-control" placeholder="Username">
+                </div>
+                <div class="mb-3">
+                    <input type="password" name="password" class="form-control" placeholder="Password">
+                </div>
+
+                <button type="submit" class="btn login-form-submit" name="login_user">Submit</button>
+
+                <div class="login-register">Not a member yet? <a href="register.php">Register ME</a></div>
+            </form>
+        </div>
+    </div>
+
+    <!-- SCRIPT -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
+        crossorigin="anonymous"></script>
+    <script src="app.js"></script>
 </body>
+
 </html>
