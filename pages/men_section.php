4e3a72fd18dd890976e9d0cc5855a87117ea1047
KhantRaj <77373424+KhantRaj@users.noreply.github.com>
2021-02-24 17:02:07 +0530

    women_section pages  and men_section pages is added

---
+++ pages/men_section.php
@@ -0,0 +1,179 @@
+<?php include('../admin/partials/header.php'); ?>
+
+<!-- NAVBAR SECTION -->
+<div id="navbar">
+    <div class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="women_section.php">Women</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="men_section.php">Men</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- MEN SECTION -->
+
+<div id="men-section">
+
+    <h2 class="section-heading">MEN'S</h2>
+    <div class="row card-collection">
+
+        <div class="card col-lg-3 col-md-6">
+            <div class="img-wrapper">
+                <img src="../img/men/men1.jpg" class="card-img" alt="men1">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$499 <br>
+                    <strong>30% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="Retro-Pants (M)">Add to
+                        cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-md-6">
+            <div class="img-wrapper">
+                <img src="../img/men/men2.jpg" class="card-img" alt="men2">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$699<br>
+                    <strong>69% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="Sweaters (M)">Add to cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-md-6">
+            <div class="img-wrapper">
+                <img src="../img/men/men1.jpg" class="card-img" alt="men1">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$499 <br>
+                    <strong>30% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="Retro-Pants (M)">Add to
+                        cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-md-6">
+            <div class="img-wrapper">
+                <img src="../img/men/men2.jpg" class="card-img" alt="men2">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$699<br>
+                    <strong>69% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="Sweaters (M)">Add to cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-md-6">
+            <div class="img-wrapper">
+                <img src="../img/men/men3.jpg" class="card-img" alt="men3">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$749<br>
+                    <strong>40% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="Grey-Jacket (M)">Add to
+                        cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-md-6">
+            <div class="img-wrapper">
+                <img src="../img/men/men1.jpg" class="card-img" alt="men1">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$499 <br>
+                    <strong>30% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="Retro-Pants (M)">Add to
+                        cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-md-6">
+            <div class="img-wrapper">
+                <img src="../img/men/men2.jpg" class="card-img" alt="men2">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$699<br>
+                    <strong>69% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="Sweaters (M)">Add to cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-md-6">
+            <div class="img-wrapper">
+                <img src="../img/men/men1.jpg" class="card-img" alt="men1">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$499 <br>
+                    <strong>30% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="Retro-Pants (M)">Add to
+                        cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-md-6">
+            <div class="img-wrapper">
+                <img src="../img/men/men2.jpg" class="card-img" alt="men2">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$699<br>
+                    <strong>69% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="Sweaters (M)">Add to cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-md-6">
+            <div class="img-wrapper">
+                <img src="../img/men/men1.jpg" class="card-img" alt="men1">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$499 <br>
+                    <strong>30% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="Retro-Pants (M)">Add to
+                        cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-md-6">
+            <div class="img-wrapper">
+                <img src="../img/men/men2.jpg" class="card-img" alt="men2">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$699<br>
+                    <strong>69% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="Sweaters (M)">Add to cart</button></a>
+            </div>
+        </div><div class="card col-lg-3 col-md-6">
+            <div class="img-wrapper">
+                <img src="../img/men/men1.jpg" class="card-img" alt="men1">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$499 <br>
+                    <strong>30% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="Retro-Pants (M)">Add to
+                        cart</button></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+        
+
+
+<?php include('../admin/partials/footer.php'); ?>
---
+++ pages/women_section.php
@@ -0,0 +1,181 @@
+<?php include('../admin/partials/header.php'); ?>
+
+<!-- NAVBAR SECTION -->
+<div id="navbar">
+    <div class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                        <a class="nav-link active" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="women_section.php">Women</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="men_section.php">Men</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- WOMEN SECTION -->
+
+<div id="women-section">
+
+    <h2 class="section-heading">WOMEN'S</h2>
+    <div class="row card-collection">
+
+        <div class="card col-lg-3 col-sm-6">
+            <div class="img-wrapper">
+                <img src="../img/women/women1.jpg" class="card-img" alt="women1">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$499 <br>
+                    <strong>30% OFF</strong>
+                </h5>
+                <a href="cart.php"> <button class="add-to-cart" value="Red-Frok (F)">Add to cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-sm-6">
+            <div class="img-wrapper">
+                <img src="../img/women/women2.jpg" class="card-img" alt="women2">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$699<br>
+                    <strong>69% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="Black-Top (F)">Add to cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-sm-6">
+            <div class="img-wrapper">
+                <img src="../img/women/women3.jpg" class="card-img" alt="women3">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$749<br>
+                    <strong>40% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="Pink-Frok (F)">Add to cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-sm-6">
+            <div class="img-wrapper">
+                <img src="../img/women/women4.jpg" class="card-img" alt="women4">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$899<br>
+                    <strong>10% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="White-Hoodie (F)">Add to
+                        cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-sm-6">
+            <div class="img-wrapper">
+                <img src="../img/women/women4.jpg" class="card-img" alt="women4">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$899<br>
+                    <strong>10% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="White-Hoodie (F)">Add to
+                        cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-sm-6">
+            <div class="img-wrapper">
+                <img src="../img/women/women4.jpg" class="card-img" alt="women4">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$899<br>
+                    <strong>10% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="White-Hoodie (F)">Add to
+                        cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-sm-6">
+            <div class="img-wrapper">
+                <img src="../img/women/women4.jpg" class="card-img" alt="women4">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$899<br>
+                    <strong>10% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="White-Hoodie (F)">Add to
+                        cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-sm-6">
+            <div class="img-wrapper">
+                <img src="../img/women/women4.jpg" class="card-img" alt="women4">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$899<br>
+                    <strong>10% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="White-Hoodie (F)">Add to
+                        cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-sm-6">
+            <div class="img-wrapper">
+                <img src="../img/women/women4.jpg" class="card-img" alt="women4">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$899<br>
+                    <strong>10% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="White-Hoodie (F)">Add to
+                        cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-sm-6">
+            <div class="img-wrapper">
+                <img src="../img/women/women4.jpg" class="card-img" alt="women4">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$899<br>
+                    <strong>10% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="White-Hoodie (F)">Add to
+                        cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-sm-6">
+            <div class="img-wrapper">
+                <img src="../img/women/women4.jpg" class="card-img" alt="women4">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$899<br>
+                    <strong>10% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="White-Hoodie (F)">Add to
+                        cart</button></a>
+            </div>
+        </div>
+        <div class="card col-lg-3 col-sm-6">
+            <div class="img-wrapper">
+                <img src="../img/women/women4.jpg" class="card-img" alt="women4">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">$899<br>
+                    <strong>10% OFF</strong>
+                </h5>
+                <a href="cart.php"><button class="add-to-cart" value="White-Hoodie (F)">Add to
+                        cart</button></a>
+            </div>
+        </div> 
+    </div>
+
+</div>
+
+<?php include('../admin/partials/footer.php'); ?>
